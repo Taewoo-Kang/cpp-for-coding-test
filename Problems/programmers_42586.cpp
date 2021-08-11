2ee6a37b23f1f2f4f01adf2b1f4bcc95285135fa
taewoo_kang <xodn0304@gmail.com>
2021-08-12 02:45:19 +0900

    기능개발 문제 풀이

---
+++ Problems/programmers_42586.cpp
@@ -0,0 +1,48 @@
+// 기능개발
+// 출처: https://programmers.co.kr/learn/courses/30/lessons/42586
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    queue<int> q;
+    for (int progress : progresses) {
+        q.push(progress);
+    }
+
+    int idx = 0;
+    int dayCnt = 0;
+    while (!q.empty()) {
+        int taskCnt = 0;
+        while (!q.empty() && q.front() + speeds[idx] * dayCnt >= 100) {
+            ++taskCnt;
+            q.pop();
+            ++idx;
+        }
+        if (taskCnt > 0) {
+            answer.push_back(taskCnt);
+        }
+        ++dayCnt;
+    }
+
+    return answer;
+}
+
+void print(vector<int> progresses, vector<int> speeds, vector<int> answer) {
+    vector<int> result = solution(progresses, speeds);
+    if (result == answer)        
+        cout << "Correct!" << endl;
+    else        
+        cout << "Wrong Answer" << endl;
+}
+ 
+int main() {
+    print({93, 30, 55}, {1, 30, 5}, {2, 1});
+    print({95, 90, 99, 99, 80, 99}, {1, 1, 1, 1, 1, 1}, {1, 3, 2});
+    return 0;
+}
